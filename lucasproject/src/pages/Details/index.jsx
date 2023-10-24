0d028fbec1757845a5092255f9eb8f2a39ab3fa2
lucas_simoes <lukas.sps19@gmail.com>
2023-10-24 15:20:48 -0300

    lucas committ

---
+++ lucasproject/src/pages/Details/index.jsx
@@ -0,0 +1,16 @@
+
+import { Container } from './styles'
+
+export function Details() {
+
+
+    return (
+        <container>
+            <h1>hello word</h1>
+            <span>
+                Rodrigo Gonçalves
+            </span>
+
+        </container>
+    )
+}
---
+++ lucasproject/src/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-componentes';
+
+
+export default createGlobalStyle`
+    * {
+
+      margin:0;
+      padding:0;
+      box-sizing: border-box;
+}
+
+ body {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color:${({ theme }) => theme.COLORS.WHITE};
+ }
+
+ a {
+    text-decoration: none;
+
+ }
+
+ button, a {
+    cursor: pointer;
+transition: filter 0.2s;
+}
+
+button, a {
+ filter:brightness(0.9);
+}
+
+
+
+`;
