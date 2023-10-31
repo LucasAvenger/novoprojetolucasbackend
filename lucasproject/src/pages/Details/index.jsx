e0b8649b70f467c673dd684c4c404a24329fab99
lucas_simoes <lukas.sps19@gmail.com>
2023-10-31 15:43:29 -0300

    adição de modoficações

--- lucasproject/src/pages/Details/index.jsx
+++ lucasproject/src/pages/Details/index.jsx
@@ -1,13 +1,8 @@
-
-<<<<<<< HEAD
-
-
-import { Container } from './styles';
+import { Container } from './styles.js';
 
 import { Button } from '../../components/Button';
-=======
-import { Container } from './style'
->>>>>>> d6ab3fb102badab0d1d6831273440dc4fd8b49f3
+
+
 
 export function Details() {
 
