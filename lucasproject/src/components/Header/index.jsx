9153c1effff30f61fd9907357e71681926c145c6
lucas_simoes <lukas.sps19@gmail.com>
2023-11-19 22:00:31 -0300

    novo commit

--- lucasproject/src/components/Header/index.jsx
+++ lucasproject/src/components/Header/index.jsx
@@ -3,6 +3,12 @@ import { Container } from './styles';
 export function Header() {
     return (
         <Container>
+            <Profile>
+                <img
+                    src="https://github.com/LucasAvenger"
+                    alt="foto de usuário" />
+
+            </Profile>
 
         </Container>
     );
