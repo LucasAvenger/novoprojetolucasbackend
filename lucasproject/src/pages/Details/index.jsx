a488389ea30edf2f800a8be608bbeb513575d720
lucas_simoes <lukas.sps19@gmail.com>
2023-12-02 00:28:01 -0300

    novo commit

--- lucasproject/src/pages/Details/index.jsx
+++ lucasproject/src/pages/Details/index.jsx
@@ -4,6 +4,7 @@ import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
 import { Tag } from '../../components/Tag';
+import { Button } from '../../components/ButtonText';
 
 
 export function Details() {
@@ -12,6 +13,7 @@ export function Details() {
     return (
         <Container>
             <Header />
+            <ButtonText title="Excluir nota" />
             <Section title="links úteis">
                 <Links>
                     <li><a href="#">https://www.rocketseat.com.br</a></li>
