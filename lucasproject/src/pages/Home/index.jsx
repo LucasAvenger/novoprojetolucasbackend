e039add5b584f1d056af50846ab1542907b26531
lucas_simoes <lukas.sps19@gmail.com>
2023-12-12 00:55:40 -0300

    new

--- lucasproject/src/pages/Home/index.jsx
+++ lucasproject/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Note } from '../../components/Note';
@@ -23,7 +23,7 @@ export function Home() {
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" />
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
 
       <Content>
